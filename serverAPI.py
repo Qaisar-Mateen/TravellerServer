0aa8af55430c05859a798687ac434ac1bcf1ef63
Qaisar Mateen <qaisarmateen313@gmail.com>
2024-08-11 00:36:21 +0500

    Update serverAPI.py

--- serverAPI.py
+++ serverAPI.py
@@ -62,4 +62,4 @@ def getCities():
     return cities.to_json(orient='records') 
 
 if __name__ == "__main__":
-    app.run(debug= True, host="0.0.0.0", port=5000)
+    app.run(debug= False, host="0.0.0.0")
